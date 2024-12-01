fb2f86142523ddd3b6cca2b07726d9500b7d80bc
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-11-30 18:57:45 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='opac3',
+    version='1.0.0',
+    description='Molecular property prediction',
+    author='Etinosa Osaro',
+    affiliation='Colon Lab @ University of Notre Dame',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'torch',
+        'torchvision',
+        'torch_geometric',
+        'ase',
+        'matplotlib',
+        'tqdm',
+        'pybel',
+    ],
+)
