70391ccad9343899964777086b244af8b793ec96
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-11-30 19:17:18 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+## make a mininconda if not already avaialable
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+source ~/miniconda3/bin/activate
+
+## software depends heavily on openbabel ##
+conda create -n openbabel_env
+conda activate openbabel_env
+conda install conda-forge::openbabel
+
